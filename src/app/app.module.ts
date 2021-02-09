9e673a6349c44234333db847893ba4e3ce2e8e97
Januario86 <januario86fp@gmail.com>
2021-02-09 17:42:12 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
+
   ],
   imports: [
     BrowserModule
