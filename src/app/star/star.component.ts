4a687bdd2f602a8369f12c6fb93d8c2e1fb6f643
Januario86 <januario86fp@gmail.com>
2021-02-09 19:07:20 -0300

    Add Rating of Courses

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -13,6 +13,6 @@ export class StarComponent implements OnChanges {
   starWidth: number;
 
   ngOnChanges():void{
-      this.starWidth = this.rating * 94 / 5;
+      this.starWidth = this.rating * 74 / 5;
   }
 }
